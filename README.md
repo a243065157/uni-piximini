d323ef0f4d8355f111e47f6b1e18965cfa1cd635
某人 <243065157@qq.com>
2024-10-04 21:03:37 +0800

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 某人
+ * @Date: 2024-10-04 20:53:13
+-->
+## pixi-demo
+pixi-miniprogram引入失败的案例
+## 运行
+`npm install`
+`npm run dev:mp-weixin`
+打开开发者工具导入`dist/dev/mp-weixin`，APPID随便用一个测试的就好
---
+++ src/pages/index/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <view class="container">
+    <canvas type="webgl" canvas-id="myCanvas" />
+  </view>
+</template>
+<script setup>
+  // import { createPIXI } from '../../libs/pixi.miniprogram'
+  const { createPIXI } = require('../../libs/pixi-miniprogram')
+</script>
